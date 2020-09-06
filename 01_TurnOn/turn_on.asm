3c90493c62470b45f5ae79633305955431fa5ca0
Paulo Bruno <paulobruno@alu.ufc.br>
2020-09-06 00:59:35 -0300

    Fix typos

--- 01_TurnOn/turn_on.asm
+++ 01_TurnOn/turn_on.asm
@@ -1,22 +1,20 @@
-; nome da secao tanto faz
-; ter essa section eh obrigatorio
-; precisa comecar em $100
+; the name of the section doesn't matter
+; this section is mandatory
+; it needs to start at address $100
 SECTION "Entry", ROM0[$100]
 
-    ; precisa pular pra outra label 
+    ; this jump is also mandatory
     nop
     jp Start
 
 
-; the header section is created by rgbfix
-; but it expects that the header section is zeroed
-; to ensure this, we just add zeros in it
-; the "SECTION "Header", ROM0" line is optional;
-;   but helps with code organization
+; the header section is actually created by rgbfix
+; but it expects the header section to be filled with zeros
+; the "SECTION "Header", ROM0" line is optional
 SECTION "Header", ROM0
 
-    ; we could also use "ds $150 - $104", which is common 
-    ;   is many ROMs, since ds will be filled with the 
+    ; we could also use "ds $150 - $104", which is common
+    ;   in many ROMs, since ds will be filled with the
     ;   value of -p rgbfix argument
     ;   https://rednex.github.io/rgbds/rgbasm.5.html
     REPT $150 - $104
@@ -24,8 +22,8 @@ SECTION "Header", ROM0
     ENDR
 
 
-; ter essa section eh obrigatorio
+; having a section before the start label is mandatory
 SECTION "Main", ROM0
 
-; label pode ter qualquer nome
+; the name of the label doesn't matter 
 Start:
