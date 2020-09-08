9bc81832122bc0aa9edd834dbb1948a7c37cfa98
Paulo Bruno <paulobruno@alu.ufc.br>
2020-09-07 23:46:52 -0300

    Point to address $8800

--- 02_ImageOnVram/manual_copy.asm
+++ 02_ImageOnVram/manual_copy.asm
@@ -14,8 +14,8 @@ SECTION "Header", ROM0
 SECTION "Main", ROM0
 
 Start:
-    ; $9000 is one of the VRAM addresses
-    ld hl, $9000
+    ; $8800 is one of the VRAM addresses
+    ld hl, $8800
 
     ; copy to VRAM
 
