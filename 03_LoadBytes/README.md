c3da659dddb64a86f66fb26e47f386d86a1cf619
Paulo Bruno <paulobruno@alu.ufc.br>
2020-09-09 17:48:50 -0300

    Add load spite from file example

--- 03_LoadBytes/README.md
+++ 03_LoadBytes/README.md
@@ -1,7 +1,20 @@
-In the codes from [Image on VRAM](../02_ImageOnVram) we loaded the sprite bytes line by line. We are now gonna see a most elegant way to load sequential bytes using the `db` instruction.
+*Note: `db` and `INCBIN` may not be recognized by all assemblers. I'm using [RGBDS](github.com/rednex/rgbds).*
 
-*Note: `db` may not be recognized by all assemblers. I'm using [RGBDS](github.com/rednex/rgbds).*
+
+### Using **db** instruction
+
+In the codes from [Image on VRAM](../02_ImageOnVram) we loaded the sprite bytes line by line. We are now gonna see a most elegant way to load sequential bytes using the `db` instruction.
 
 ![Loading bytes using db](define_bytes.png)
 
 As we can see, we get the same result as before, but with a much cleaner code.
+
+
+### Loading from file
+
+Better than having all the sprite bytes wrote into the code, we can define them in files and load them from the file.  
+This can be achieved using the `INCBIN` instruction.
+
+![Loading from file](load_file.png)
+
+Again, we get the same result.
---
+++ 03_LoadBytes/load_file.asm
@@ -0,0 +1,59 @@
+SECTION "Entry", ROM0[$100]
+    nop
+    jp Start
+
+
+SECTION "Header", ROM0
+    REPT $150 - $104
+        db 0
+    ENDR
+
+
+SECTION "Main", ROM0
+
+Start:
+
+.waitVBlank 
+    ld a, [$FF44]
+    cp 144
+    jp c, .waitVBlank
+    
+    ; turn off lcd
+    xor a
+    ld [$FF40], a
+
+    ; $8800 is one of the VRAM addresses
+    ld hl, $8800
+
+    ; we use 'de' to point to the start of the data
+    ld de, BlackSpriteStart
+
+    ; we use 'bc' to store the size of the data
+    ld bc, BlackSpriteEnd - BlackSpriteStart
+
+; we loop through the data, loading the value into 'hl'
+.copyToVram
+    ld a, [de]
+    ld [hli], a ; ld [hl], a ; inc hl
+    inc de
+    dec bc
+    
+    ; while bc != 0, keep copying
+    ld a, b
+    or c
+    jr nz, .copyToVram
+
+    ; turn on lcd
+    ld a, %10010001
+    ld [$FF40], a
+
+.lock
+    jr .lock
+
+
+; we can also use the INCBIN instruction to load the bytes from file
+; the file should contain only the sprite bytes
+; it doesn't matter, but usually sprites have the '.2bpp' extension
+BlackSpriteStart:
+    INCBIN "black_sprite.2bpp"
+BlackSpriteEnd:
